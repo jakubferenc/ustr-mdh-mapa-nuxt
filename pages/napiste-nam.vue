77a52671be1002b4a389e37fbb7ecd03e6986efc
Jakub Ferenc <jakub@assemblage.cz>
2021-12-02 12:10:21 +0100

    Create empty page

---
+++ pages/napiste-nam.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+
+  .page
+
+
+</template>
+
+<style lang="sass">
+
+
+</style>
+<script>
+
+export default {
+
+
+    async asyncData ({store}) {
+
+
+
+    },
+
+    computed: {
+
+
+    },
+
+    mounted() {
+
+
+    },
+
+    data() {
+      return {
+        maps: {},
+        title: `Napište nám`,
+      }
+    },
+
+    head () {
+      return {
+        title: `${this.title} — ${this.$config.globalTitle}`,
+        htmlAttrs: {
+          class: 'index'
+        }
+      }
+    }
+
+}
+</script>
