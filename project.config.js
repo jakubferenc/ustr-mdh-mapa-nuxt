62ec2a8bef1e5d1af8a584bdcce388c94f23e78c
Jakub Ferenc <jakub@assemblage.cz>
2022-01-25 20:55:03 +0100

    Correct type in the app name

--- project.config.js
+++ project.config.js
@@ -7,7 +7,7 @@ const fullhd = 1344 + 2 * gap;
 const mobile = tablet-1;
 
 export default {
-  globalTitle: 'MHD Mapa',
+  globalTitle: 'Muzeum dělnického hnutí — Mapa',
   appSlug: 'mdh-mapa',
   firebaseConfig: {
     config: {
